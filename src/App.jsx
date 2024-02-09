9e3b183f7a006f2ea086e00f1a66f7aaab6ec312
rupokghosh <rupokghosh291@gmail.com>
2024-02-09 11:48:54 -0800

    import mui & set up initial components and pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
-
+import Header from "./components/Header";
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import { Button } from "@mui/material";
 
 function App() {
   return (
     <div>
-      <h1>bookify</h1>
+      <Header/>
+      
     </div>
   );
 }
