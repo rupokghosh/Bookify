51c494ce3c0fa82212f2e31f162bcb14dc989bd8
rupokghosh <rupokghosh291@gmail.com>
2024-02-09 13:55:38 -0800

    basic header styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,47 @@
+import { Box } from "@mui/material";
 import React from "react";
+import { Link } from "react-router-dom";
+import SearchIcon from "@mui/icons-material/Search";
+import ShoppingBagIcon from "@mui/icons-material/ShoppingBag";
 
 function Header() {
-  return <h1>hi</h1>;
+  return (
+    <>
+      <Box
+        sx={[
+          { display: "flex" },
+          { justifyContent: "space-between" },
+          { alignItems: "center" },
+          { bgcolor: "#944E63" },
+          { padding: "2rem" },
+        ]}
+      >
+        <Link style={{ textDecoration: "none" }} to="/">
+          <h2>Bookify</h2>
+        </Link>
+
+        <Box
+          sx={[{ gap: "2rem" }, { display: "flex" }, { marginRight: "3rem" }]}
+        >
+          <Link to="sales" underline="hover">
+            {"On Sale"}
+          </Link>
+          <Link to="allBooks" underline="hover">
+            {"All Books"}
+          </Link>
+          <Link to="newReleases" underline="hover">
+            {"New Releases"}
+          </Link>
+          <Link to="search" underline="hover">
+            <SearchIcon />
+          </Link>
+          <Link to="cart" underline="hover">
+            <ShoppingBagIcon />
+          </Link>
+        </Box>
+      </Box>
+    </>
+  );
 }
 
 export default Header;
